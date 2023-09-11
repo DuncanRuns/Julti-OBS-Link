f542e276d5ca6376c4ba93643bcc102b9b237627
draconix6 <30545768+draconix6@users.noreply.github.com>
2023-09-12 10:15:31 +1200

    fixed possible 6 in data string

--- src/instance.lua
+++ src/instance.lua
@@ -40,7 +40,7 @@ function set_instance_data(num, lock_visible, dirt_cover, freeze_active, x, y, w
         obs.obs_source_filter_add(source, filter)
     end
     if not (filter == nil) then
-        obs.obs_source_set_enabled(filter, freeze_active and not lock_visible)
+        obs.obs_source_set_enabled(filter, freeze_active)
     end
 
     -- Instance Group: position
@@ -50,14 +50,14 @@ function set_instance_data(num, lock_visible, dirt_cover, freeze_active, x, y, w
 end
 
 function set_instance_data_from_string(instance_num, data_string)
-    -- data_string format: lock/cover state (1 = lock, 2 = cover, 3 = both, 4 = freeze, 5 = freeze & lock, 5 = freeze & cover (TODO: fix?), 0 = neither), x, y, w, h
+    -- data_string format: lock/cover state (1 = lock, 2 = cover, 3 = both, 4 = freeze, 5 = freeze & lock (just lock), 0 = neither), x, y, w, h
     -- Example: "2,0,0,960,540"
     local nums = split_string(data_string, ",")
     set_instance_data(
         instance_num,                                               -- instance number
         (nums[1] == "1") or (nums[1] == "3") or (nums[1] == "5"),   -- lock visible
-        (nums[1] == "2") or (nums[1] == "3") or (nums[1] == "6"),   -- cover visible
-        (nums[1] == "4") or (nums[1] == "5") or (nums[1] == "6"),   -- freeze filter active
+        (nums[1] == "2") or (nums[1] == "3"),                       -- cover visible
+        (nums[1] == "4"),                                           -- freeze filter active
         tonumber(nums[2]),                                          -- x
         tonumber(nums[3]),                                          -- y
         tonumber(nums[4]),                                          -- width
