82c63fe878f63a07ff8de3ad98a837bedb670c45
DuncanRuns <59705125+DuncanRuns@users.noreply.github.com>
2023-09-14 23:07:40 -0300

    Explicit bits for state format

--- src/instance.lua
+++ src/instance.lua
@@ -50,17 +50,35 @@ function set_instance_data(num, lock_visible, dirt_cover, freeze_active, x, y, w
 end
 
 function set_instance_data_from_string(instance_num, data_string)
-    -- data_string format: lock/cover state (1 = lock, 2 = cover, 3 = both, 4 = freeze, 5 = freeze & lock (just lock), 0 = neither), x, y, w, h
+    -- data_string format: lock/cover state (flag bits: locked, dirt cover, freeze filter), x, y, w, h
     -- Example: "2,0,0,960,540"
     local nums = split_string(data_string, ",")
+
+    local flagBits = tonumber(nums[1])
+
+    local freezeActive = flagBits >= 4
+    if freezeActive then
+        flagBits = flagBits - 4
+    end
+
+    local coverVisible = flagBits >= 2
+    if coverVisible then
+        flagBits = flagBits - 2
+    end
+
+    local lockVisible = flagBits >= 1
+    if lockVisible then
+        flagBits = flagBits - 1
+    end
+
     set_instance_data(
-        instance_num,                                               -- instance number
-        (nums[1] == "1") or (nums[1] == "3") or (nums[1] == "5"),   -- lock visible
-        (nums[1] == "2") or (nums[1] == "3"),                       -- cover visible
-        (nums[1] == "4"),                                           -- freeze filter active
-        tonumber(nums[2]),                                          -- x
-        tonumber(nums[3]),                                          -- y
-        tonumber(nums[4]),                                          -- width
-        tonumber(nums[5])                                           -- height
+        instance_num,      -- instance number
+        lockVisible,       -- lock visible
+        coverVisible,      -- cover visible
+        freezeActive,      -- freeze filter active
+        tonumber(nums[2]), -- x
+        tonumber(nums[3]), -- y
+        tonumber(nums[4]), -- width
+        tonumber(nums[5])  -- height
     )
 end
